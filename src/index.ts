f2d9bfbae2e8bc92ac4db150e7f9b3684f9a9381
ShaunLWM <shaunlimweimin@gmail.com>
2021-09-14 09:42:21 +0800

    Add more variations

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,29 @@ const wait = (ms = 2000) => new Promise((res) => setTimeout(res, ms));
 (async () => {
   let canContinue = true;
   let page = 1;
-  const arr = [];
+  const minimal = [];
+  const full = [];
+  const top200m = [];
+  const top500m = [];
+
   while (canContinue) {
     const response = await fetch(
       `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=${page}`
     );
     const items = (await response.json()) as MarketItem[];
-    arr.push(...items.map((p) => pick(p, ["id", "name", "symbol", "image"])));
+    full.push(...items);
     console.log(`Finished page ${page}`);
     if (items.length < 250) canContinue = false;
     page += 1;
     await wait();
   }
 
-  fs.writeFileSync("minimal_marketcap_desc.json", JSON.stringify(arr, null, 2));
+  minimal.push(...full.map((p) => pick(p, ["id", "name", "symbol", "image"])));
+  top200m.push(...full.map((p) => pick(p, ["id", "name", "symbol", "image"])).slice(0, 200));
+  top500m.push(...full.map((p) => pick(p, ["id", "name", "symbol", "image"])).slice(0, 500));
+
+  fs.writeFileSync("minimal_marketcap_desc.json", JSON.stringify(minimal, null, 2));
+  fs.writeFileSync("minimal_marketcap_desc_top200.json", JSON.stringify(top200m, null, 2));
+  fs.writeFileSync("minimal_marketcap_desc_top500.json", JSON.stringify(top500m, null, 2));
+  fs.writeFileSync("full_marketcap_desc.json", JSON.stringify(full, null, 2));
 })();
